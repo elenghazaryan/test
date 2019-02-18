bbaa5da3c2a69950056ee8ff7b058872db359f0a
Elen Ghazaryan <elen.ghazaryan@softconstruct.local>
2019-02-18 16:50:49 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+def arithmetic(a, b, s):
+    if s == '+':
+        return '{a}+{b}={sum}'.format(a=a, b=b, sum=a + b)
+    elif s == '-':
+        return 'a-b={}'.format(a - b)
+    elif s == '/':
+        if b != 0:
+            return 'a/b={}'.format(a / b)
+        else:
+            return "unknown operation"
+    elif s == '*':
+        return 'a*b={}'.format(a * b)
+    else:
+        return "unknown operation"
+
+
+def square(a):
+    p = a * 4
+    an = (2 ** 0.5) * a
+    s = a * a
+    return p, an, s
+
+
+# while True:
+#     try:
+#         a = int(input())
+#         b = int(input())
+#         s = input()
+#         break
+#     except ValueError:
+#         print("type error")
+#
+#
+# print(arithmetic(a, b, s))
+
+while True:
+    try:
+        a = int(input())
+        break
+    except ValueError:
+        print("type error")
+
+print(square(a))
